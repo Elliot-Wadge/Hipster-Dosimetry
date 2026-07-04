7f304dbca68b02db2e1065adaa386111e17f8c8f
Elliot Wadge <elliotwadge@gmail.com>
2026-07-04 16:26:29 -0400

    remove an unecessary funcition call and reorder some logic

--- src/HipsterDosimetry/optimizer.py
+++ src/HipsterDosimetry/optimizer.py
@@ -23,24 +23,30 @@ def newton_raphson(x0, ODs, a, b, c, tol, bound, max_iter=100):
             # if we have a sign change already the bisect
             if bracketed:
                 new_x = 0.5 * (bracket[0] + bracket[1])
-            # if no sign change yet then start with a random
+            # if no sign change yet then start with a random new x in the bound
             else:
                 new_x = bracket[0] + np.random.random() * (bracket[1] - bracket[0])
         else:
             new_x = x - score / dfx
         
-        new_score = objective_derivative(new_x, ODs, a, b, c)
         # if the new_x is outside of the bounds and we have a sign change then use bisection
         if (new_x < bracket[0] or new_x > bracket[1]) and bracketed:
             new_x =  (bracket[0] + bracket[1])/2
         # if the new_x is outside of the bound and we don't have a sign change then select random x in range
         elif not bracketed and (new_x < bracket[0] or new_x > bracket[1]):
             new_x = bracket[0] + np.random.random()*(bracket[1] - bracket[0])
-        # in case new_x has changed update the score
+        # update the score
         new_score = objective_derivative(new_x, ODs, a, b, c)
 
-        # if we have found a sign change, update our bound and set flag
-        if not bracketed and new_score * score < 0:
+
+        # update the bounds based on new_x
+        if bracketed:
+            if objective_derivative(bracket[0], ODs, a, b, c) * new_score < 0:
+                bracket[1] = new_x
+            else:
+                bracket[0] = new_x
+        # if we have found a sign change for the first time, update our bound and set flag
+        elif not bracketed and new_score * score < 0:
             if x < new_x:
                 bracket[0] = x
                 bracket[1] = new_x
@@ -48,12 +54,7 @@ def newton_raphson(x0, ODs, a, b, c, tol, bound, max_iter=100):
                 bracket[0] = new_x
                 bracket[1] = x
             bracketed = True
-        # update the bounds based on new_x
-        if bracketed:
-            if objective_derivative(bracket[0], ODs, a, b, c) * new_score < 0:
-                bracket[1] = new_x
-            else:
-                bracket[0] = new_x
+        
 
         x = new_x
         score = new_score
