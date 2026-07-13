18df11f98d361655bb0ee37eb756609aef9718bd
Elliot Wadge <elliotwadge@gmail.com>
2026-07-13 15:55:07 -0400

    implement easy averaging of images, add some more images for comparison

--- src/HipsterDosimetry/util.py
+++ src/HipsterDosimetry/util.py
@@ -6,20 +6,35 @@ def combine_tif_images(dir:str):
     p = Path(dir)
     
     track_dictionary = {}
-    for file in p.glob('*.tif'):
-        name = str(file).split('.tif')[0][:-3]
-        exists = track_dictionary.get(name)
-        
-        if exists:
-            continue
+    all_files = list(p.glob('*.tif'))
+    save_path = Path(dir + "combined/")
+    save_path.mkdir(parents=True, exist_ok=True)
+    
 
-        print(f'{name}[0-9][0-9][0-9].tif')
-        matches = p.glob(f'*.tif')
-        print(list(matches))
-        images = np.array([ski.io.imread(match) for match in matches])
+
+    for file in all_files:
+        base_name = file.stem[:-3]
+
+
+        if base_name in track_dictionary:
+            continue
+        track_dictionary[base_name] = True
+        print(f'{base_name}.tif')
+        matches = [
+            f for f in all_files 
+            if f.stem.startswith(base_name) and f.stem[-3:].isdigit()
+        ]
+        
+        print(f"Found {len(matches)} matches: {[f.name for f in matches]}")
+        
+        if not matches:
+            continue
+        print(matches)
+        images = np.array([ski.io.imread(str(match)) for match in matches])
+        print(images.shape)
         combined_image = np.mean(images, axis=0)
         print(combined_image.shape)
-        ski.io.imsave(f'{name}_combined.tif', combined_image)
+        ski.io.imsave(save_path / Path(f'{base_name}_combined.tif'), combined_image)
 
 
 if __name__ == '__main__':
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from HipsterDosimetry.util import combine_tif_images
+
+if __name__ == '__main__':
+    dir = 'scans/measurements/'
+    combine_tif_images(dir)
